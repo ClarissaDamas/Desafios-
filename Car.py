020cb6518ce6e47e5fd2a4e067b3671e8fd324b2
clarissaD <physicscla@gmail.com>
2026-03-04 08:22:15 -0300

    Create Car.py

---
+++ Car.py
@@ -0,0 +1,19 @@
+MOTIVO:
+Novas funcionalidades em sua plataforma de APIs. Para conseguir realizar a simulação de compras é necessário integrar a API da tabela FIPE em seu sistema para que seus cliente consigam consultar o valores dos veículos.
+
+Integrar as APIs de Marcas, Modelos e Preços;
+Melhorar a segurança
+Fluxo de segurança;
+________
+1. Configuração do Postman.
+Criar workspace FIPE;
+
+Criar a collection FIPE;
+
+Criar um diretório /test/v1;
+
+Criar um diretório /models/v1;
+
+Criar um diretório /brands/v1;
+
+Criar um diretório /pricing/v1;
